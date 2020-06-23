23a72e81034aefc105ee78266d9340487c4f2e4b
Austin Mac <amac@roblox.com>
2020-06-23 16:42:55 -0700

    aggregate requested job resource

--- main.go
+++ main.go
@@ -3,66 +3,76 @@ package main
 import (
 	// "database/sql"
 	"encoding/json"
-	"errors"
+	// "errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	// "strconv"
 	"sync"
-
+	"time"
 	// _ "github.com/mattn/go-sqlite3"
 )
 
 var wg sync.WaitGroup
 
-type JobData struct {
+type JobData struct { 
 	JobID string
-	Data map[string] interface{}
+	CPU float64
+	memoryMB float64
+	diskMB float64
+	IOPS float64
 }
+ 
+func aggResources(address, jobID string, e chan error) (float64, float64, float64, float64) {
+	var CPUTotal, memoryMBTotal, diskMBTotal, IOPSTotal float64
 
-func getJobResources(address, jobID string, e chan error) map[string]interface{} {
 	api := "http://" + address + "/v1/job/" + jobID
 	response, errHttp := http.Get(api)
 
-	// errHttp = errors.New("HTTP ERROR - getJobResources(address, jobID, e)")
+	// errHttp = errors.New("HTTP ERROR - aggResources(address, jobID, e)")
 	if errHttp != nil {
 		e <- errHttp
 	}
 
 	data, errIoutil := ioutil.ReadAll(response.Body)
-
-	// errIoutil = errors.New("IOUTIL ERROR - getJobResources(address, jobID, e)")
+	
+	// errIoutil = errors.New("IOUTIL ERROR - aggResources(address, jobID, e)")
 	if errIoutil != nil {
 		e <- errIoutil
 	}
 
 	jobJSON := string(data)
+	var jobSpec map[string]interface{}
+	json.Unmarshal([]byte(jobJSON), &jobSpec)
 
-	var result map[string]interface{}
+	taskGroups := jobSpec["TaskGroups"].([]interface{})
+	for _, taskGroup := range taskGroups {
+		count := taskGroup.(map[string]interface{})["Count"].(float64)
+		tasks := taskGroup.(map[string]interface{})["Tasks"].([]interface{})
+		for _, task := range tasks {
+			resources := task.(map[string]interface{})["Resources"].(map[string]interface{})
+			CPUTotal += count * resources["CPU"].(float64)
+			memoryMBTotal += count * resources["MemoryMB"].(float64)
+			diskMBTotal += count * resources["DiskMB"].(float64)
+			IOPSTotal += count * resources["IOPS"].(float64)
+		}
+	}
+	return CPUTotal, memoryMBTotal, diskMBTotal, IOPSTotal
+}
 
-	json.Unmarshal([]byte(jobJSON), &result)
-
-	// unpacking JSON
-	taskGroups := result["TaskGroups"].([]interface{})[0]
-	tasks := taskGroups.(map[string]interface{})["Tasks"].([]interface{})[0]
-	resources := tasks.(map[string]interface{})["Resources"].(map[string]interface{})
-
-	return resources
-} 
-
-func accessJobs(v string, c chan []JobData, e chan error) {
-	api := "http://" + v + "/v1/jobs" 
+func reachCluster(address string, c chan []JobData, e chan error) {
+	api := "http://" + address + "/v1/jobs" 
 	response, errHttp := http.Get(api)
 
-	// errHttp = errors.New("HTTP ERROR - accessJobs(v, c, e)")
+	// errHttp = errors.New("HTTP ERROR - reachCluster(address, c, e)")
 	if errHttp != nil {
 		e <- errHttp
 	}
 	
 	data, errIoutil := ioutil.ReadAll(response.Body)
 	
-	// errIoutil = errors.New("IOUTIL ERROR - accessJobs(v, c, e)")
+	// errIoutil = errors.New("IOUTIL ERROR - reachCluster(address, c, e)")
 	if errIoutil != nil {
 		e <- errIoutil
 	}
@@ -76,7 +86,8 @@ func accessJobs(v string, c chan []JobData, e chan error) {
 
 	for i := range keys {
 		jobID := keys[i].(map[string]interface{})["JobSummary"].(map[string]interface{})["JobID"] // unpack JobID from JSON
-		jobDataSlice = append(jobDataSlice, JobData{jobID.(string), getJobResources(v, jobID.(string), e)})
+		CPUTotal, memoryMBTotal, diskMBTotal, IOPSTotal := aggResources(address, jobID.(string), e)
+		jobDataSlice = append(jobDataSlice, JobData{jobID.(string), CPUTotal, memoryMBTotal, diskMBTotal, IOPSTotal})
 	}
 	c <- jobDataSlice
 	wg.Done()
@@ -85,7 +96,6 @@ func accessJobs(v string, c chan []JobData, e chan error) {
 func main() {
 	// will be provided 1 address/cluster
 	addresses := []string{"***REMOVED***:***REMOVED***", "***REMOVED***:***REMOVED***"} // substitute for config file, server address
-	// addresses := []string{}
 	buffer := len(addresses)
 	c := make(chan []JobData, buffer)
 	e := make(chan error)
@@ -93,40 +103,34 @@ func main() {
 
 	go func(e chan error) {
 		err := <-e
-		// close(e)
 		log.Fatal("Error: ", err)	
 	}(e)
 	
-	for _, v := range addresses {
+	start := time.Now()
+	for _, address := range addresses {
 		wg.Add(1)
-		go accessJobs(v, c, e)
+		go reachCluster(address, c, e)
 	}
 
 	wg.Wait()
 	close(c)
-
-	// select {
-	// 	case <-c:
-	// 		break
-	// 	case err := <-e:
-	// 		close(e)
-	// 		fmt.Println("Error: ", err)
-	// 		log.Fatal("Error: ", err)
-	// }
+	end := time.Now()
 	
 	for jobDataSlice := range c {
 		for _, v := range jobDataSlice {
 			m[v.JobID] = v
 		}
 	}
-	
 
+	// may not have to use hash table for aggregation, duplicate filter
+	// since aggregation is done in aggResources()
+	// and duplicates should be filtered out by separate cluster addresses
 	i := 0
-	for key, val := range m {
-		fmt.Println(i, ":", key)
-		fmt.Println(val)
+	for _, val := range m {
+		fmt.Println(i, ":", val)
 		i += 1
 	}
 
 	fmt.Println("Complete.")
+	fmt.Println("Elapsed:", end.Sub(start))
 }
