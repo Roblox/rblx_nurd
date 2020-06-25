b78cb41be8a24f6209749c8b4f59919777486848
Austin Mac <amac@roblox.com>
2020-06-25 10:04:24 -0700

    aggregate usage stats

--- getAllocStats.go
+++ getAllocStats.go
@@ -9,6 +9,8 @@ import (
 )
 
 func aggUsageResources(address, jobID string) {
+	var rssUsageTotal, totalTicksUsageTotal float64
+
 	jobsAPI := "http://" + address + "/v1/job/" + jobID + "/allocations"
 	response, _ := http.Get(jobsAPI)
 	data, _ := ioutil.ReadAll(response.Body)
@@ -16,6 +18,8 @@ func aggUsageResources(address, jobID string) {
 
 	keys := make([]interface{}, 0)
 	json.Unmarshal(sliceOfAllocs, &keys)
+
+	
 
 	// prints out alloc ids for a specified job
 	for i := range keys {
@@ -27,9 +31,32 @@ func aggUsageResources(address, jobID string) {
 		var allocStats map[string]interface{}
 		json.Unmarshal([]byte(string(allocData)), &allocStats)
 
-		fmt.Println(allocID)
-		fmt.Println(allocStats["ResourceUsage"], "\n")
+		resourceUsage := allocStats["ResourceUsage"].(map[string]interface{})
+		memoryStats := resourceUsage["MemoryStats"].(map[string]interface{})
+		cpuStats := resourceUsage["CpuStats"].(map[string]interface{})
+
+		rss := memoryStats["RSS"]
+		cache := memoryStats["Cache"]
+		swap := memoryStats["Swap"]
+
+		userMode := cpuStats["UserMode"]
+		totalTicks := cpuStats["TotalTicks"]
+
+		fmt.Println(allocID, "\n--- Memory Stats")
+		fmt.Println("	RSS:", rss)
+		fmt.Println("	Cache:", cache)
+		fmt.Println("	Swap:", swap)
+
+		fmt.Println("--- CPU Stats")
+		fmt.Println("	User Mode:", userMode)
+		fmt.Println("	Total Ticks:", totalTicks, "\n")
+
+		rssUsageTotal += rss.(float64)
+		totalTicksUsageTotal += totalTicks.(float64)
 	}
+
+	fmt.Println("Total RSS Usage:", rssUsageTotal)
+	fmt.Println("Total Ticks Usage:", totalTicksUsageTotal)
 
 }
 
