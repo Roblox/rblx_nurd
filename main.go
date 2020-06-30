b0dec7276181d3693b3f2fd606b2e612348dc484
Austin Mac <amac@roblox.com>
2020-06-30 11:43:42 -0700

    correct usage data collection from prometheus

--- main.go
+++ main.go
@@ -22,9 +22,11 @@ var wg sync.WaitGroup
 // resource data for a job
 type JobData struct { 
 	JobID string
+	name string
 	uTicks float64
 	rCPU float64
 	uRSS float64
+	pRSS float64
 	rMemoryMB float64
 	rdiskMB float64
 	rIOPS float64
@@ -33,9 +35,49 @@ type JobData struct {
 	currentTime string
 }
 
+func getAllocIDFromProm(address string) map[string]string {
+	api := "http://" + address + "/api/v1/query?query=nomad_client_allocs_memory_allocated_value"
+	response, _ := http.Get(api)
+
+	raw, _ := ioutil.ReadAll(response.Body)
+	var allocs map[string]interface{}
+	json.Unmarshal([]byte(string(raw)), &allocs)
+
+	data := allocs["data"].(map[string]interface{})
+	result := data["result"].([]interface{})
+
+	// fmt.Println(result[0].(map[string]interface{})["metric"].(map[string]interface{})["job"])
+	m := make(map[string]string)
+	for _, v := range result {
+		alloc_id := v.(map[string]interface{})["metric"].(map[string]interface{})["alloc_id"].(string)
+		m[alloc_id] = "value"
+	}
+	return m
+}
+
+func getAllocIDFromNomadByJobID(address, jobID string) map[string]string {
+	api := "http://" + address + "/v1/job/" + jobID + "/allocations"
+	response, _ := http.Get(api)
+	data, _ := ioutil.ReadAll(response.Body)
+
+	sliceOfAllocs := []byte(string(data))
+	keys := make([]interface{}, 0)
+	json.Unmarshal(sliceOfAllocs, &keys)
+
+	m := make(map[string]string)
+
+	for i := range keys {
+		allocID := keys[i].(map[string]interface{})["ID"].(string)
+		m[allocID] = "value"
+	}
+
+	return m
+}
+
 // Aggregate total CPU, memory usage for a job
-func aggUsageResources(address, jobID string, e chan error) (float64, float64) {
-	var ticksTotal, rssTotal, cacheTotal, swapTotal, usageTotal, maxUsageTotal, kernelUsageTotal, kernelMaxUsageTotal float64
+func aggUsageResources(address, jobID, name string, e chan error) (float64, float64, float64) {
+	var ticksTotal, rssTotal, cacheTotal, swapTotal, usageTotal, maxUsageTotal, kernelUsageTotal, kernelMaxUsageTotal, rssProm float64
+	promAllocs := getAllocIDFromProm("***REMOVED***:***REMOVED***")
 
 	api := "http://" + address + "/v1/job/" + jobID + "/allocations"
 	response, errHttp := http.Get(api)
@@ -90,7 +132,38 @@ func aggUsageResources(address, jobID string, e chan error) (float64, float64) {
 		}
 	}
 
-	return ticksTotal, rssTotal 
+	nomadAllocs := getAllocIDFromNomadByJobID(address, jobID)
+
+	for key := range nomadAllocs {
+		if _, ok := promAllocs[key]; !ok { // if key is not in promAllocs
+			clientAllocAPI := "http://" + address + "/v1/client/allocation/" + key + "/stats"
+			allocResponse, _ := http.Get(clientAllocAPI)
+			allocData, _ := ioutil.ReadAll(allocResponse.Body)
+			var allocStats map[string]interface{}
+			json.Unmarshal([]byte(string(allocData)), &allocStats)
+
+			if allocStats["ResourceUsage"] != nil {
+				resourceUsage := allocStats["ResourceUsage"].(map[string]interface{})
+				memoryStats := resourceUsage["MemoryStats"].(map[string]interface{})
+				
+				rssProm += memoryStats["RSS"].(float64) / 1.049e6
+			}
+
+		}
+	}
+	// Here: sum remaining usage stats from prom
+	// rssTotal += sum(prom RSS)
+	promAPI := "http://***REMOVED***:***REMOVED***/api/v1/query?query=sum(nomad_client_allocs_memory_rss_value%7Bjob%3D%22" + name + "%22%7D)%20by%20(job)"
+	promResponse, _ := http.Get(promAPI)
+	promData, _ := ioutil.ReadAll(promResponse.Body)
+	var promStats map[string]interface{}
+	json.Unmarshal([]byte(string(promData)), &promStats)
+	if len(promStats["data"].(map[string]interface{})["result"].([]interface{})) != 0 {
+		num, _ := strconv.ParseFloat(promStats["data"].(map[string]interface{})["result"].([]interface{})[0].(map[string]interface{})["value"].([]interface{})[1].(string), 64)
+		rssProm += num / 1.049e6
+	}
+
+	return ticksTotal, rssTotal, rssProm
 }
 
 // Aggregate resources requested by a job
@@ -153,7 +226,8 @@ func reachCluster(address string, c chan []JobData, e chan error) {
 
 	for i := range keys {
 		jobID := keys[i].(map[string]interface{})["JobSummary"].(map[string]interface{})["JobID"].(string) // unpack JobID from JSON
-		ticksUsage, rssUsage := aggUsageResources(address, jobID, e)
+		name := keys[i].(map[string]interface{})["Name"].(string)
+		ticksUsage, rssUsage, rssProm := aggUsageResources(address, jobID, name, e)
 		CPUTotal, memoryMBTotal, diskMBTotal, IOPSTotal := aggReqResources(address, jobID, e)
 		namespace := keys[i].(map[string]interface{})["JobSummary"].(map[string]interface{})["Namespace"].(string)
 		dataCentersSlice := keys[i].(map[string]interface{})["Datacenters"].([]interface{})
@@ -167,9 +241,11 @@ func reachCluster(address string, c chan []JobData, e chan error) {
 		currentTime := time.Now().Format("2006-01-02 15:04:05")
 		jobData := JobData{
 						jobID, 
+						name, 
 						ticksUsage, 
 						CPUTotal, 
-						rssUsage, 
+						rssUsage,
+						rssProm, 
 						memoryMBTotal, 
 						diskMBTotal, 
 						IOPSTotal,
@@ -211,6 +287,7 @@ func initDB(nameDB string) (*sql.DB, *sql.Stmt) {
 		uTicks REAL,
 		rCPU REAL, 
 		uRSS REAL,
+		pRSS REAL,
 		rMemoryMB REAL,
 		rdiskMB REAL,
 		rIOPS REAL,
@@ -227,12 +304,13 @@ func initDB(nameDB string) (*sql.DB, *sql.Stmt) {
 		uTicks, 
 		rCPU,
 		uRSS,
+		pRSS,
 		rMemoryMB,
 		rdiskMB,
 		rIOPS,
 		namespace,
 		dataCenters,
-		date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+		date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
 
 	if errInsert != nil {
 		log.Fatal("Error:", errInsert)
@@ -245,12 +323,12 @@ func printRowsDB(db *sql.DB) {
 	rows, _ := db.Query("SELECT * FROM resources")
 
 	var JobID, namespace, dataCenters, currentTime string
-	var uTicks, rCPU, uRSS, rMemoryMB, rdiskMB, rIOPS float64
+	var uTicks, rCPU, uRSS, rMemoryMB, rdiskMB, rIOPS, pRSS float64
 	var id int
 
 	for rows.Next() {
-		rows.Scan(&id, &JobID, &uTicks, &rCPU, &uRSS, &rMemoryMB, &rdiskMB, &rIOPS, &namespace, &dataCenters, &currentTime)
-		fmt.Println(strconv.Itoa(id) + ": ", JobID, uTicks, rCPU, uRSS, rMemoryMB, rdiskMB, rIOPS, namespace, dataCenters, currentTime)
+		rows.Scan(&id, &JobID, &uTicks, &rCPU, &uRSS, &pRSS, &rMemoryMB, &rdiskMB, &rIOPS, &namespace, &dataCenters, &currentTime)
+		fmt.Println(strconv.Itoa(id) + ": ", JobID, uTicks, rCPU, uRSS, pRSS, rMemoryMB, rdiskMB, rIOPS, namespace, dataCenters, currentTime)
 	}
 }
 
@@ -291,6 +369,7 @@ func main() {
 					v.uTicks,
 					v.rCPU,
 					v.uRSS,
+					v.pRSS,
 					v.rMemoryMB,
 					v.rdiskMB,
 					v.rIOPS,
@@ -301,7 +380,7 @@ func main() {
 		}
 
 		printRowsDB(db)
-		
+		// this prints rows fo the 
 		fmt.Println("Elapsed:", end.Sub(begin))
 		time.Sleep(duration)
 	}
