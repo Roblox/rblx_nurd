1d415ec151954ed8ca735a5e90f8d02594dd055a
Austin Mac <amac@roblox.com>
2020-07-27 15:39:57 -0700

    use assert

--- config_test.go
+++ config_test.go
@@ -1,48 +1,25 @@
 package main
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadConfig(t *testing.T) {
 	err := loadConfig("NOPATH")
-	if err == nil {
-		t.Errorf("Value incorrect, got: %v, want %v", err, "open NOPATH: no such file or directory")
-	}
-	if len(nomadAddresses) != 0 {
-		t.Errorf("Length incorrect, got: %v, want %v", len(nomadAddresses), 0)
-	}
-	if metricsAddress != "" {
-		t.Errorf("Value incorrect, got: %v, want %v", metricsAddress, "")
-	}
+	assert.Error(t, err)
+	assert.Empty(t, nomadAddresses)
+	assert.Empty(t, metricsAddress)
 
 	err = loadConfig("config_test.json")
-	if err != nil {
-		t.Errorf("Value incorrect, got: %v, want %v", err, "<nil>")
-	}
-	if len(nomadAddresses) != 2 {
-		t.Errorf("Length incorrect, got: %v, want %v", len(nomadAddresses), 2)
-	}
-	if reflect.TypeOf(nomadAddresses).Kind() != reflect.Slice {
-		t.Errorf("Type incorrect, got: %T, want %v", nomadAddresses, "slice")
-	}
-	if reflect.TypeOf(nomadAddresses[0]).Kind() != reflect.String {
-		t.Errorf("Type incorrect, got: %T, want %v", nomadAddresses[0], "string")
-	}
-	if nomadAddresses[0] != "NomadURL0:NomadPort0" {
-		t.Errorf("Value incorrect, got: %v, want %v", nomadAddresses[0], "NomadURL0:NomadPort0")
-	}
-	if reflect.TypeOf(nomadAddresses[1]).Kind() != reflect.String {
-		t.Errorf("Type incorrect, got: %T, want %v", nomadAddresses[1], "string")
-	}
-	if nomadAddresses[1] != "NomadURL1:NomadPort1" {
-		t.Errorf("Value incorrect, got: %v, want %v", nomadAddresses[1], "NomadURL1:NomadPort1")
-	}
-	if reflect.TypeOf(metricsAddress).Kind() != reflect.String {
-		t.Errorf("Type incorrect, got: %T, want %v", metricsAddress, "string")
-	}
-	if metricsAddress != "VMURL:VMPort" {
-		t.Errorf("Value incorrect, got: %v, want %v", metricsAddress, "VMURL:VMPort")
-	}
+	assert.Empty(t, err)
+	assert.Equal(t, 2, len(nomadAddresses))
+	assert.IsType(t, []string{}, nomadAddresses)
+	assert.IsType(t, "", nomadAddresses[0])
+	assert.Equal(t, "NomadURL0:NomadPort0", nomadAddresses[0])
+	assert.IsType(t, "", nomadAddresses[1])
+	assert.Equal(t, "NomadURL1:NomadPort1", nomadAddresses[1])
+	assert.IsType(t, "", metricsAddress)
+	assert.Equal(t, "VMURL:VMPort", metricsAddress)
 }
