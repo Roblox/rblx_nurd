f14f74241d0dafb3a16797ed95ff9649ae67a165
Austin Mac <amac@roblox.com>
2020-07-28 14:17:48 -0700

    more tests
    Signed-off-by: Austin Mac <amac@roblox.com>

--- main_test.go
+++ main_test.go
@@ -5,10 +5,6 @@ import (
 	"net/http/httptest"
 	"testing"
 
-<<<<<<< HEAD
-	"github.com/DATA-DOG/go-sqlmock"
-=======
->>>>>>> unit tests for main
 	"github.com/stretchr/testify/assert"
 
 	// "fmt"
@@ -24,61 +20,19 @@ func TestHomePage(t *testing.T) {
 }
 
 func TestReturnAll(t *testing.T) {
-<<<<<<< HEAD
-	var mock sqlmock.Sqlmock
-	var err error
-	db, mock, err = sqlmock.New()
-	assert.Empty(t, err)
-	defer db.Close()
-
-	// Empty DB
-	rows := sqlmock.NewRows([]string{"id", "JobID", "name", "uTicks", "rCPU", "uRSS", "uCache", "rMemoryMB", "rdiskMB", "rIOPS", "namespace", "dataCenters", "date", "insertTime"})
-	mock.ExpectQuery(`SELECT \* FROM resources`).WillReturnRows(rows)
-
-	request, err := http.NewRequest("GET", "/v1/jobs", nil)
-=======
 	request, err := http.NewRequest("GET", "/v1/job", nil)
->>>>>>> unit tests for main
 	assert.Empty(t, err)
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(returnAll)
 	handler.ServeHTTP(rr, request)
 	assert.Equal(t, http.StatusOK, rr.Code)
-<<<<<<< HEAD
-
-	// fmt.Println("Result:", rr.Body)
-
-	// Non-empty in DB
-	rows = sqlmock.NewRows([]string{"id", "JobID", "name", "uTicks", "rCPU", "uRSS", "uCache", "rMemoryMB", "rdiskMB", "rIOPS", "namespace", "dataCenters", "date", "insertTime"}).
-		AddRow(1, "JobID1", "name1", 111.1, 111.1, 111.1, 111.1, 111.1, 111.1, 111.1, "namespace1", "dataCenter1", "0000-00-01", "0000-00-01").
-		AddRow(2, "JobID2", "name2", 222.2, 222.2, 222.2, 222.2, 222.2, 222.2, 222.2, "namespace2", "dataCenter2", "0000-00-02", "0000-00-02")
-	mock.ExpectQuery(`SELECT \* FROM resources`).WillReturnRows(rows)
-
-	request, err = http.NewRequest("GET", "/v1/jobs", nil)
-	assert.Empty(t, err)
-	rr = httptest.NewRecorder()
-	handler = http.HandlerFunc(returnAll)
-	handler.ServeHTTP(rr, request)
-	assert.Equal(t, http.StatusOK, rr.Code)
-
-	// fmt.Println("Result:", rr.Body)
-}
-
-func TestReturnJob(t *testing.T) {
-	request, err := http.NewRequest("GET", "/v1/job/jobID", nil)
-=======
 }
 
 func TestReturnJob(t *testing.T) {
 	request, err := http.NewRequest("GET", "/v1/job/adaas-job", nil)
->>>>>>> unit tests for main
 	assert.Empty(t, err)
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(returnJob)
 	handler.ServeHTTP(rr, request)
 	assert.Equal(t, http.StatusOK, rr.Code)
-<<<<<<< HEAD
-=======
-
->>>>>>> unit tests for main
 }
