23cc66ad150f873273587b71f4498e2c4c974fa9
Austin Mac <amac@roblox.com>
2020-07-28 13:49:05 -0700

    unit tests for main
    Signed-off-by: Austin Mac <amac@roblox.com>

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	// "fmt"
+)
+
+func TestHomePage(t *testing.T) {
+	request, err := http.NewRequest("GET", "/", nil)
+	assert.Empty(t, err)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(homePage)
+	handler.ServeHTTP(rr, request)
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestReturnAll(t *testing.T) {
+	request, err := http.NewRequest("GET", "/v1/job", nil)
+	assert.Empty(t, err)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(returnAll)
+	handler.ServeHTTP(rr, request)
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestReturnJob(t *testing.T) {
+	request, err := http.NewRequest("GET", "/v1/job/adaas-job", nil)
+	assert.Empty(t, err)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(returnJob)
+	handler.ServeHTTP(rr, request)
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+}
