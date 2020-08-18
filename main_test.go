3f502fcc7c5f7e3e83e800b757c27e68eafff588
Austin Mac <amac@roblox.com>
2020-08-18 10:44:19 -0700

    read resposne
    Signed-off-by: Austin Mac <amac@roblox.com>

--- main_test.go
+++ main_test.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 
-	// "fmt"
+	"fmt"
 )
 
 func TestHomePage(t *testing.T) {
-	request, err := http.NewRequest("GET", "/", nil)
+	log.SetOutput(ioutil.Discard)
+
+	testJSON := &Disk {
+		SizeMB: 12.0,
+	}
+	json, _ := json.Marshal(testJSON)
+	request, err := http.NewRequest("POST", "/", bytes.NewBuffer(json))
+
+
+	// request, err := http.NewRequest("GET", "/", nil)
 	assert.Empty(t, err)
+	request.Header.Set("Content-Type", "application/json")
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(homePage)
 	handler.ServeHTTP(rr, request)
 	assert.Equal(t, http.StatusOK, rr.Code)
+	body, _ := ioutil.ReadAll(rr.Body)
+	fmt.Println(string(body))
+
 }
 
 func TestReturnAll(t *testing.T) {
